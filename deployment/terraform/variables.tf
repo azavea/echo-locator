6e9bf97b75ead476913e05d08849ef45f06f7ca7
Jean Cochrane <jcochrane@azavea.com>
2019-01-23 12:55:06 -0500

    Initialize Terraform config CloudFront + S3 origin
    * Create a simple Terraform configuration for CloudFront with S3 origin
    * Add docker-compose.ci.yml and scripts/infra for running infrastructure
        updates
    * Add infrastructure deployment documentation

---
+++ deployment/terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "project" {
+  default = "EchoLocator"
+}
+
+variable "environment" {
+  default = "Staging"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "r53_public_hosted_zone_name" {}
