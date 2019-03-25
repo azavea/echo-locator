870c6e6a6856bf17e05b298ec770a0d1f02bf0c7
Kathryn Killebrew <kkillebrew@azavea.com>
2019-03-25 11:26:28 -0400

    Make neighborhood details a component
    Add some elements, including toggleable star.

--- taui/src/components/neighborhood-details.js
+++ taui/src/components/neighborhood-details.js
@@ -1,17 +1,86 @@
 // @flow
+import Icon from '@conveyal/woonerf/components/icon'
 import message from '@conveyal/woonerf/message'
+import {PureComponent} from 'react'
 
-export default function NeighborhoodDetails ({neighborhood}) {
-  if (!neighborhood) {
-    return null
+export default class NeighborhoodDetails extends PureComponent<Props> {
+  props: Props
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      isFavorite: false
+    }
+    this.setFavorite = this.setFavorite.bind(this)
   }
 
-  console.log('Show details for neighborhood:')
-  console.log(neighborhood)
+  setFavorite () {
+    console.log('TODO: #33 save/unsave neighborhood to favorites')
+    this.setState({isFavorite: !this.state.isFavorite})
+  }
 
-  return (
-    <div className='Card'>
-      <h2>Hello details</h2>
-    </div>
-  )
+  render () {
+    const { neighborhood } = this.props
+    const { isFavorite } = this.state
+
+    if (!neighborhood) {
+      return null
+    }
+
+    const { id, town } = neighborhood.properties
+    const { time } = neighborhood
+
+    const bestJourney = neighborhood.segments && neighborhood.segments.length
+      ? neighborhood.segments[0] : null
+
+    const setFavorite = this.setFavorite
+
+    return (
+      <div className='Card'>
+        <div className='CardTitle'>
+          <Icon type={isFavorite ? 'star' : 'star-o'}
+            onClick={setFavorite}
+            style={{cursor: 'pointer'}} />
+          <span>{town} - {id}</span>
+        </div>
+        <table className='CardContent'>
+          <tbody>
+            <tr className='BestTrip'>
+              <td><span className='fa fa-street-view' /></td>
+              <td>
+                {bestJourney.map((segment, index) => (
+                  <Segment key={index} segment={segment} />
+                ))}
+                {time > 120
+                  ? <span className='decrease'>inaccessible within 120 minutes</span>
+                  : <span>in
+                    <strong> {time}</strong> {message('Units.Mins')}
+                  </span>}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <br />
+        <div>
+          <a href='' target='_blank'>Look a link</a>
+        </div>
+      </div>
+    )
+  }
 }
+
+// FIXME: copied from route-segments
+const Segment = ({segment}) => (
+  <span
+    className='CardSegment'
+    style={{
+      backgroundColor: segment.backgroundColor || 'inherit',
+      color: segment.color || 'inherit',
+      textShadow: `0 0 1px ${segment.color === '#fff' ? '#333' : '#fff'}`
+    }}
+    title={segment.name}
+  >
+    <i className={`fa fa-${segment.type}`} /> {segment.name}
+  </span>
+)
