591e61bd0f8898e1c37eeb00a3de9fe08d785283
Kathryn Killebrew <kkillebrew@azavea.com>
2019-04-05 12:23:50 -0400

    Add script to download images and fetch metadata
    Get metadata needed for attribution from the Wikipedia API for images from there, and
    download thumbnail versions of the images.
    
    Connects #88.

---
+++ neighborhood_data/fetch_images.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python
+# encoding=utf8
+
+"""
+Fetch image metadata and download thumbnails.
+"""
+
+import csv
+import errno
+import os
+import requests
+
+DESCRIPTIONS_CSV = 'neighborhood_descriptions.csv'
+OUTPUT_FILE = 'neighborhood_extended_descriptions.csv'
+
+if not os.path.isfile(DESCRIPTIONS_CSV):
+    print('\nMissing neighborhood images and descriptions in {f}.\n\n'.format(
+        f=DESCRIPTIONS_CSV))
+    raise IOError(errno.ENOENT,
+                  os.strerror(errno.ENOENT),
+                  DESCRIPTIONS_CSV)
+
+IMAGE_DIRECTORY = 'images/'
+IMAGE_FIELDS = ['school', 'open_space_or_landmark', 'street', 'town_square']
+IMAGE_METADATA_FIELDS = [
+    'thumbnail',
+    'license',
+    'license_url',
+    'description',
+    'artist',
+    'username'
+]
+
+METADATA_URL = 'https://en.wikipedia.org/w/api.php'
+
+
+def get_image_metadata(url, type):
+    print('Get metadata for image {u} of type {t}...\n'.format(u=url, t=type))
+    offset = url.find('File:')
+
+    wiki_offset = url.find('wikimedia.org')
+    if wiki_offset == -1:
+        wiki_offset = url.find('wikipedia.org')
+
+    if offset > -1 and wiki_offset > -1:
+        filename = url[offset:]
+    elif offset == -1 and wiki_offset > -1:
+        # Read out end of URL path to get the file name
+        offset = url.rfind('/')
+        if url.lower().endswith('.jpg'):
+            # Have a Wikipedia image link, but without File: prepended.
+            # Strip any translated File: prefix, then prepend File: to look up
+            # from English metadata API
+            endpath = url[offset + 1:]
+            offset = endpath.find(':')
+            if offset > -1:
+                endpath = endpath[offset + 1:]
+            filename = 'File:' + endpath
+        else:
+            print('Wikipedia article link: {u}'.format(u=url))
+            return
+    else:
+        print('URL {u} of type {t} has no Wikipedia File reference.\n'.format(
+            u=url, t=type))
+        return
+
+    params = {
+        'action': 'query',
+        'prop': 'imageinfo',
+        'iiprop': 'user|userid|extmetadata|url',
+        'iiurlwidth': '120',
+        'iiurlheight': '90',
+        'format': 'json',
+        'titles': filename
+    }
+    r = requests.get(METADATA_URL, params=params)
+    if not r.ok:
+        print('request failed!')
+        return
+    resp = r.json()
+    page = resp['query']['pages'].keys()[0]
+
+    imageinfo = (resp['query']['pages'][page]['imageinfo'][0]
+                 if 'imageinfo' in resp['query']['pages'][page] else '')
+    if not imageinfo:
+        print('Got no imageinfo for {u}'.format(u=url))
+    metadata = imageinfo['extmetadata'] if imageinfo else ''
+    username = imageinfo['user'] if imageinfo else ''
+    thumbnail = imageinfo['thumburl'] if imageinfo else ''
+    artist = metadata['Artist']['value'] if 'Artist' in metadata else ''
+    license_name = metadata['LicenseShortName']['value'] if 'LicenseShortName' in metadata else ''
+    license_url = (metadata['LicenseUrl']['value']
+                   if 'LicenseUrl' in metadata else '')
+    description = metadata['ObjectName']['value'] if 'ObjectName' in metadata else ''
+    return {
+        'thumbnail': thumbnail,
+        'license': license_name,
+        'license_url': license_url,
+        'description': description,
+        'artist': artist,
+        'username': username
+    }
+
+
+def get_image_metadata_fields(fld, data):
+    extended = {}
+    if not data:
+        for f in IMAGE_METADATA_FIELDS:
+            extended['{field}_{key}'.format(field=fld, key=f)] = ''
+    else:
+        for key in data.keys():
+            extended['{field}_{key}'.format(field=fld, key=key)] = data[key]
+    return extended
+
+
+def download_image(url, filename):
+    """Return true on success."""
+    print('Download image to {filename}...'.format(filename=filename))
+    r = requests.get(url, stream=True)
+    if r.status_code == 200:
+        with open(filename, 'w') as imagefile:
+            for chunk in r.iter_content(chunk_size=128):
+                imagefile.write(chunk)
+        return True
+    else:
+        print('Failed to download image from {url}. Response: {status}.'.format(
+            url=url, status=r.status_code))
+    return False
+
+
+def get_image_filename(zipcode, fieldname, thumbnail):
+    lthumb = thumbnail.lower()
+    offset = lthumb.rfind('.')
+    extension = lthumb[offset:]
+    return os.path.join(IMAGE_DIRECTORY,
+                        '{zipcode}_{fld}{extension}'.format(zipcode=zipcode,
+                                                            fld=fieldname,
+                                                            extension=extension))
+
+
+# Fields in DESCRIPTIONS_CSV:
+# zip_code,town,town_website_description,town_link,wikipedia,wikipedia_link,street,school,
+# town_square,open_space_or_landmark,extra
+with open(DESCRIPTIONS_CSV) as df:
+    rdr = csv.DictReader(df)
+    fieldnames = list(rdr.fieldnames)
+    descriptions = [r for r in rdr]
+
+extended_fieldnames = fieldnames
+for img in IMAGE_FIELDS:
+    for meta in IMAGE_METADATA_FIELDS:
+        extended_fieldnames.append(
+            '{field}_{meta}'.format(field=img, meta=meta))
+
+# Track counts
+good_urls = bad_urls = missing_urls = 0
+downloaded_images = failed_downloads = skipped_downloads = 0
+
+with open(OUTPUT_FILE, 'w') as outf:
+    wtr = csv.DictWriter(outf, fieldnames=extended_fieldnames)
+    wtr.writeheader()
+    for row in descriptions:
+        zipcode = row['zip_code'].zfill(5)
+        print('\n\nProcessing {zip}...'.format(zip=zipcode))
+        extended_row = row
+        for fld in IMAGE_FIELDS:
+            url = row[fld]
+            metadata = None
+            if url:
+                metadata = get_image_metadata(url, fld)
+                if not metadata or not metadata['thumbnail']:
+                    bad_urls += 1
+                else:
+                    good_urls += 1
+                    thumb = metadata['thumbnail']
+                    filename = get_image_filename(zipcode, fld, thumb)
+                    exists = os.path.isfile(filename)
+                    if exists:
+                        skipped_downloads += 1
+                    elif download_image(thumb, filename):
+                        downloaded_images += 1
+                    else:
+                        failed_downloads += 1
+            else:
+                missing_urls += 1
+            extended_row.update(get_image_metadata_fields(fld, metadata))
+        wtr.writerow(extended_row)
+
+print('Good: {g} Bad: {b} Missing: {m}'.format(g=good_urls,
+                                               b=bad_urls,
+                                               m=missing_urls))
+
+print('\n\nDownloaded {d} images. Attempted and failed to download {f} images.'.format(
+    d=downloaded_images, f=failed_downloads))
+
+print('Skipped downloading {s} images already in {images_dir}.'.format(
+    s=skipped_downloads,
+    images_dir=IMAGE_DIRECTORY)
+)
