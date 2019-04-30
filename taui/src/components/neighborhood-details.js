1b8d85b600545de8d5d6500d1f1ce9aa3175f7aa
Kathryn Killebrew <kkillebrew@azavea.com>
2019-04-30 15:34:16 -0400

    Put search links on separate line

--- taui/src/components/neighborhood-details.js
+++ taui/src/components/neighborhood-details.js
@@ -131,57 +131,61 @@ export default class NeighborhoodDetails extends PureComponent<Props> {
       ',' + neighborhood.geometry.coordinates[0]
 
     return (
-      <div className='neighborhood-details__links'>
-        {neighborhood.properties.town_link && <a
-          className='neighborhood-details__link'
-          href={neighborhood.properties.town_link}
-          target='_blank'
-        >
-          {message('NeighborhoodDetails.WebsiteLink')}
-        </a>}
-        <a
-          className='neighborhood-details__link'
-          href={getCraigslistSearchLink(
-            neighborhood.properties.id,
-            userProfile.rooms)}
-          target='_blank'
-        >
-          {message('NeighborhoodDetails.CraigslistSearchLink')}
-        </a>
-        <a
-          className='neighborhood-details__link'
-          href={getZillowSearchLink(
-            neighborhood.properties.id,
-            userProfile.rooms)}
-          target='_blank'
-        >
-          {message('NeighborhoodDetails.ZillowSearchLink')}
-        </a>
-        {neighborhood.properties.wikipedia_link && <a
-          className='neighborhood-details__link'
-          href={neighborhood.properties.wikipedia_link}
-          target='_blank'
-        >
-          {message('NeighborhoodDetails.WikipediaLink')}
-        </a>}
-        <a
-          className='neighborhood-details__link'
-          href={getGoogleSearchLink(neighborhood.properties.id)}
-          target='_blank'
-        >
-          {message('NeighborhoodDetails.GoogleSearchLink')}
-        </a>
-        <a
-          className='neighborhood-details__link'
-          href={getGoogleDirectionsLink(
-            originCoordinateString,
-            destinationCoordinateString,
-            hasVehicle)}
-          target='_blank'
-        >
-          {message('NeighborhoodDetails.GoogleMapsLink')}
-        </a>
-      </div>
+      <>
+        <div className='neighborhood-details__links'>
+          {neighborhood.properties.town_link && <a
+            className='neighborhood-details__link'
+            href={neighborhood.properties.town_link}
+            target='_blank'
+          >
+            {message('NeighborhoodDetails.WebsiteLink')}
+          </a>}
+          {neighborhood.properties.wikipedia_link && <a
+            className='neighborhood-details__link'
+            href={neighborhood.properties.wikipedia_link}
+            target='_blank'
+          >
+            {message('NeighborhoodDetails.WikipediaLink')}
+          </a>}
+          <a
+            className='neighborhood-details__link'
+            href={getGoogleSearchLink(neighborhood.properties.id)}
+            target='_blank'
+          >
+            {message('NeighborhoodDetails.GoogleSearchLink')}
+          </a>
+          <a
+            className='neighborhood-details__link'
+            href={getGoogleDirectionsLink(
+              originCoordinateString,
+              destinationCoordinateString,
+              hasVehicle)}
+            target='_blank'
+          >
+            {message('NeighborhoodDetails.GoogleMapsLink')}
+          </a>
+        </div>
+        <div className='neighborhood-details__links'>
+          <a
+            className='neighborhood-details__link'
+            href={getCraigslistSearchLink(
+              neighborhood.properties.id,
+              userProfile.rooms)}
+            target='_blank'
+          >
+            {message('NeighborhoodDetails.CraigslistSearchLink')}
+          </a>
+          <a
+            className='neighborhood-details__link'
+            href={getZillowSearchLink(
+              neighborhood.properties.id,
+              userProfile.rooms)}
+            target='_blank'
+          >
+            {message('NeighborhoodDetails.ZillowSearchLink')}
+          </a>
+        </div>
+      </>
     )
   }
 
