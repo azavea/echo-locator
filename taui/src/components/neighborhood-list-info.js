4b70b0eb6dbf4d70856d16afbeabaf2c8cbf7484
Kathryn Killebrew <kkillebrew@azavea.com>
2019-06-06 13:38:12 -0400

    Invert range for crime quintile
    Lower is better.
    Also force out-of-range values in range by setting zeroes to 1.

--- taui/src/components/neighborhood-list-info.js
+++ taui/src/components/neighborhood-list-info.js
@@ -1,6 +1,8 @@
 // @flow
 import message from '@conveyal/woonerf/message'
 
+import scale from '../utils/scaling'
+
 import Meter from './meter'
 import RentalUnitsMeter from './rental-units-meter'
 
@@ -10,7 +12,9 @@ export default function NeighborhoodListInfo ({neighborhood}) {
   }
 
   const props = neighborhood.properties
-  const crimeQuintile = props['violentcrime_quintile']
+  const crime = props['violentcrime_quintile'] > 0 ? props['violentcrime_quintile'] : 1
+  // Invert range for crime quintile
+  const crimeQuintile = scale(crime, 1, 5, 4, 0)
   const edPercentile = props['education_percentile']
   const houses = props['house_number_symbol']
   const isSchoolChoice = !!props['school_choice']
@@ -27,7 +31,7 @@ export default function NeighborhoodListInfo ({neighborhood}) {
         <tr>
           <td className='neighborhood-facts__cell'>{message('NeighborhoodInfo.ViolentCrime')}</td>
           <td className='neighborhood-facts__cell'>
-            <Meter value={crimeQuintile} average={3} max={5} />
+            <Meter value={crimeQuintile} average={2} max={4} />
           </td>
         </tr>
         <tr>
