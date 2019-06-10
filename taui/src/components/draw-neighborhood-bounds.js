db2d7364ea681cecc416bc2c0f4f6ade120b7d1d
Kathryn Killebrew <kkillebrew@azavea.com>
2019-06-10 15:33:40 -0400

    Set hover style for details
    Highlight without displaying active route neighborhood on hover in details view.
    
    Connects #161.

--- taui/src/components/draw-neighborhood-bounds.js
+++ taui/src/components/draw-neighborhood-bounds.js
@@ -15,13 +15,23 @@ export default class DrawNeighborhoodBounds extends React.PureComponent {
   _key = 0
   _getKey () { return this._key++ }
 
+  hoverStyle = (feature) => {
+    return Object.assign({}, NEIGHBORHOOD_BOUNDS_STYLE, {
+      fillColor: feature.active || feature.routable
+        ? NEIGHBORHOOD_HOVER_COLOR
+        : NEIGHBORHOOD_NONROUTABLE_COLOR
+    })
+  }
+
   render () {
     const p = this.props
+    const hoverStyle = this.hoverStyle
     return <LayerGroup key={`neighborhood-boundary-${this._getKey()}`}>
       {p.neighborhoods && <VGrid
         data={p.neighborhoods}
         idField='id'
         tooltip='town'
+        hoverStyle={(feature) => hoverStyle(feature)}
         onClick={p.clickNeighborhood}
         onMouseover={p.hoverNeighborhood}
         zIndex={p.zIndex}
