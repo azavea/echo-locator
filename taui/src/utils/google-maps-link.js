74341a1274822e1e0f9e547f8da1360097e6da2c
Scott Lederer <slederer@azavea.com>
2019-06-26 09:24:09 -0400

    Show directions link for transit, too. Remove Google Maps link.

--- taui/src/utils/google-maps-link.js
+++
@@ -1,6 +0,0 @@
-// @flow
-// Returns a link to Google Maps with the given search term
-const GOOGLE_BASE_URL = 'https://www.google.com/maps/place/'
-export default function getGoogleSearchLink (query) {
-  return GOOGLE_BASE_URL + encodeURIComponent(query)
-}
