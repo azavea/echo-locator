4f01e76b19756f58ea34cf499e2a907f13fdd0ec
Kathryn Killebrew <kkillebrew@azavea.com>
2019-10-25 13:13:02 -0400

    Update Craigslist links
    Fix deep-linking into Craigslist (URL changed slightly).
    
    Fixes #266.

--- taui/src/utils/craigslist-search-link.js
+++ taui/src/utils/craigslist-search-link.js
@@ -1,6 +1,6 @@
 // @flow
 // Returns a link to Boston area Craigslist housing search for zipcode and number of bedrooms
-const CRAIGSLIST_BASE_URL = 'https://boston.craigslist.org/search/aap?postal='
+const CRAIGSLIST_BASE_URL = 'https://boston.craigslist.org/search/apa?postal='
 export default function getCraigslistSearchLink (zipcode, rooms, maxSubsidy) {
   return CRAIGSLIST_BASE_URL + encodeURIComponent(zipcode) + '&min_bedrooms=' +
     encodeURIComponent(rooms) + '&max_bedrooms=' + encodeURIComponent(rooms) +
