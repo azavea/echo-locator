5b2ad5f007ca135601706e9094333a4e60fc519b
Derek Zheng <derek_zheng@college.harvard.edu>
2020-05-18 14:49:35 -0700

    include new spreadsheet

--- taui/src/utils/google-spreadsheet.js
+++ taui/src/utils/google-spreadsheet.js
@@ -3,8 +3,10 @@
 //real: 1aTsG_fm5CGYWiM-YJkfXKzJwEVWjo7-HNQ9t1S6_jQY
 const SHEET_ID = '1-oSni5SVKOjj_9YR4LPKN7N4UgPdi3eeFA8sM-X0XNw'
 const RANGES = 'Sheet1'
-
 //or: Form+Responses+1
+
+//BHA Apartment Listing:1JVIaWGXZ71jeyjGhyTEG5S-jyzsW-UXq1HoxydLtJ-A
+
 
 import axios from 'axios'
 //GSHEET_API_KEY
