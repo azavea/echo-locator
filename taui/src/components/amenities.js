c9fe66146dd04bfbb342a4078d5365eb8d1ae7f1
jace-garcia <jcgarcia@mit.edu>
2020-06-30 16:33:12 -0400

    buttons styled without icons, amenity handleClick and handleHover, icon images added, images not loading

--- taui/src/components/amenities.js
+++ taui/src/components/amenities.js
@@ -2,37 +2,86 @@ import {Component} from 'react'
 
 import Amenity from './amenity'
 
+import childcare from '../img/icons/childcare.png'
+import park from '../img/icons/park.png'
+import grocery from '../img/icons/grocery.png'
+import school from '../img/icons/school.png'
+import playground from '../img/icons/playground.png'
+import medical from '../img/icons/medical.png'
+import library from '../img/icons/library.png'
+
 type State = {
     amenities: string[],
+}
+
+const amenityColors = {
+    'school': '#FFA000',
+    'medical': '#F27660',
+    'childcare': '#E9658D',
+    'park': '#48B265',
+    'grocery': '#4D97CA',
+    'playground': '#28B3B0',
+    'library': '#7F9DA5',
 }
 
 // Class that handles Amenity functionality
 export default class Amenities extends Component<Props, State> {
     constructor(props) {
         super(props)
+        // Final amenities list: School, Park, Playground, Childcare, Library, Medical, Grocery
+        this.state = {
+            amenities: ['School', 'Park', 'Playground', 'Childcare', 'Library', 'Medical', 'Grocery']
+        }
     }
-
-    state = {
-        amenities: ['School', 'Park']
-    }
+    
 
     getNeighborhoodAmenityLocations(amenity) {
         // TODO
     }
 
     render(){
-        console.log(this.state)
         return (
-            <div className={'ameities-box'}>
-                <Amenity
-                    name={this.state.amenities[0]}
-                    color={'red'}
-                />
-                <Amenity
-                    name={this.state.amenities[1]}
-                    color={'blue'}
-                />
+            <div>
+                <p style={{fontWeight: 'bold'}}>Features</p>
+                <div className={'amenities-box'}>
+                    <Amenity
+                        name={this.state.amenities[0]} // School
+                        color={amenityColors['school']}
+                        image={school}
+                    />
+                    <Amenity
+                        name={this.state.amenities[1]} // Park
+                        color={amenityColors['park']}
+                        image={park}
+                    />
+                    <Amenity 
+                        name={this.state.amenities[2]} // Playground
+                        color={amenityColors['playground']}
+                        image={playground}
+                    />
+                    <Amenity 
+                        name={this.state.amenities[3]} // Childcare
+                        color={amenityColors['childcare']}
+                        image={childcare}
+                    />
+                    <Amenity 
+                        name={this.state.amenities[4]} // Library
+                        color={amenityColors['library']}
+                        image={library}
+                    />
+                    <Amenity 
+                        name={this.state.amenities[5]} // Medical
+                        color={amenityColors['medical']}
+                        image={medical}
+                    />
+                    <Amenity 
+                        name={this.state.amenities[6]} // Grocery
+                        color={amenityColors['grocery']}
+                        image={grocery}
+                    />
+                </div>
             </div>
+            
         )
     }
 }
--- taui/src/components/amenity.js
+++ taui/src/components/amenity.js
@@ -2,19 +2,53 @@ import {Component} from 'react'
 
 type Props = {
     name: string,
-    image?: string, // image url
+    image: string, // image url
     color: string,
 }
 
 export default class Amenity extends Component<Props> {
     constructor(props) {
         super(props);
+        this.state = {clicked: false, hovered: false, defaultColor: '#9AA0A6'}
+        this.handleHover = this.handleHover.bind(this)
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    
+
+    handleHover() {
+        this.setState({hovered: !this.state.hovered})
+    }
+
+    handleClick() {
+        this.setState({clicked: !this.state.clicked})
     }
 
     render() {
+        // setting up styles for button
+        var bgColor = '';
+        if (this.state.clicked) {
+            if (this.state.hovered) {
+                bgColor = this.state.defaultColor;
+            }
+            else {
+                bgColor = this.props.color;
+            }
+        }
+        else {
+            if (this.state.hovered) {
+                bgColor = this.props.color;
+            }
+            else {
+                bgColor = this.state.defaultColor;
+            }
+        }
+        var buttonStyle = {backgroundColor: bgColor, color:'white', padding: '0% 20%', width: '110%', height: '110%', fontSize:'80%'}
+
         return (
-            <div className={'amenities-amenity'}>
-                <button style={{color:this.props.color}}>
+            <div className={'amenities-amenity'} onMouseEnter={this.handleHover} onMouseLeave={this.handleHover}>
+                <button style={buttonStyle} onClick={this.handleClick}>
+                    <img src={this.props.image} alt=''></img>
                     {this.props.name}
                 </button>
             </div>
