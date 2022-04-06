8f6ffcc7a7aa716d115bff403f2c876cc6acd22f
Michael Maurizi <mmaurizi@azavea.com>
2022-04-06 14:14:34 -0400

    Enable password reset for Django admin

--- src/backend/echo/urls.py
+++ src/backend/echo/urls.py
@@ -16,9 +16,30 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 from echo import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(
+        "admin/password_reset/",
+        auth_views.PasswordResetView.as_view(),
+        name="admin_password_reset",
+    ),
+    path(
+        "admin/password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
+    path("admin/", admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
