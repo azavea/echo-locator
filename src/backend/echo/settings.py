3c4c774af8b02015e5ff20d2b7fd5176a60a6ca9
Rachele Morino <rmorino@azavea.com>
2022-04-14 16:48:54 -0400

    Make taui assets accessible for django-spa

--- src/backend/echo/settings.py
+++ src/backend/echo/settings.py
@@ -149,6 +149,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = ((os.path.join(STATIC_ROOT, 'assets')),)
 
 # Set the django-spa static file storage:
 STATICFILES_STORAGE = 'spa.storage.SPAStaticFilesStorage'
