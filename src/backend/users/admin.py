5ac7c7bf9436517e723bba22e870cc62f7eb9708
Katie <kbrown@azavea.com>
2022-04-18 15:53:59 -0400

    Change "username" to "email" in name only for user creation and editing
    This commit re-labels the username field to email to make it clear
    what should be entered; it also validates users' emails prior to saving
    a new user.

--- src/backend/users/admin.py
+++ src/backend/users/admin.py
@@ -1,10 +1,43 @@
 from django.contrib import admin
+from django import forms
+from django.core.validators import validate_email
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User, Group
+from django.contrib.auth.forms import UserCreationForm
 from .models import UserProfile, Destination
 
+# Validates that the username is a valid email on save
+class CreateUserAdminForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def clean(self):
+        cleaned_data = super(CreateUserAdminForm, self).clean()
+        try:
+            validate_email(cleaned_data['username'])
+            return cleaned_data
+        except:
+            raise forms.ValidationError('Please enter a valid email address')
+
 # Define a new UserAdmin that filters Counselor group view
+# This admin also changes the username field to be referred to as "email"
 class UserAdmin(BaseUserAdmin):
+    add_form = CreateUserAdminForm
+    list_display = ('username', 'first_name', 'last_name')
+    fieldsets = (
+        (None, {'fields': ('username', 'password', 'first_name', 'last_name')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['username'].label = 'Email:'
+        return form
+
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         if request.user.is_superuser:
