ca12711f9c784a2bfa5fc04715a2f0233d62855d
Rachele Morino <rmorino@azavea.com>
2022-05-10 16:19:34 -0400

    Initial sign up tests

--- src/backend/users/tests.py
+++ src/backend/users/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import Group, User
 from django.test import Client, TestCase
+from users.models import UserProfile
 
 
 class AdminSiteTest(TestCase):
@@ -87,3 +88,53 @@ class AdminSiteTest(TestCase):
             "/admin/login/?next=/admin/",
             f"Got unexpected response: {response.status_code}. {response.content}",
         )
+
+
+class HouseSeekerSignUpTest(TestCase):
+    """Check User sign up process."""
+
+    fixtures = ["group_permissions"]
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.houseseeker = Client()
+        cls.test_houseseeker_group = Group.objects.get(name="HouseSeeker")
+
+    def test_signup_endpoint_response(self):
+        """
+        Test signup endpoint responds with correct login message.
+        """
+        first_response = self.houseseeker.post(
+            "/api/signup/",
+            {"username": "testechoemail@azavea.com"},
+            content_type="application/json",
+        )
+        self.assertEqual(
+            first_response.status_code,
+            200,
+            f"Expected 200, got {first_response.status_code}. {first_response.content}",
+        )
+        second_response = self.houseseeker.post(
+            "/api/signup/",
+            {"username": "testechoemail@azavea.com"},
+            content_type="application/json",
+        )
+        self.assertEqual(
+            second_response.status_code,
+            200,
+            f"Expected 200, got {second_response.status_code}. {second_response.content}",
+        )
+        self.assertContains(first_response, "created your account", status_code=200)
+        self.assertContains(second_response, "already have an account", status_code=200)
+
+    def test_user_and_no_profile_created_on_signup(self):
+        self.houseseeker.post(
+            "/api/signup/",
+            {"username": "testechoemail1@azavea.com"},
+            content_type="application/json",
+        )
+        houseseeker_user = User.objects.filter(username="testechoemail1@azavea.com")
+        self.assertEqual(len(houseseeker_user), 1)
+        self.assertEqual(houseseeker_user.first().groups.first(), self.test_houseseeker_group)
+        houseseeker_user_profile = UserProfile.objects.filter(user=houseseeker_user.first())
+        self.assertIsNone(houseseeker_user_profile.first())
