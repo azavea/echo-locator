7071a90c4e7f1bb0a789db885829f7a67205edea
Aaron Su <xunzesu@outlook.com>
2022-05-11 16:53:18 -0400

    Fix bugs after rebase

--- src/frontend/src/components/custom-sign-in.js
+++ src/frontend/src/components/custom-sign-in.js
@@ -3,6 +3,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { withTranslation } from "react-i18next";
 
+import logo from "../img/echo_combined_logo_STACKED_fullcolor.svg";
+
 class SignInHeader extends React.Component {
   render() {
     const { t } = this.props;
@@ -10,11 +12,7 @@ class SignInHeader extends React.Component {
       <header className="auth-screen__header auth-header">
         <h2 className="auth-header__agency">{t("Agency")}</h2>
         <h1 className="auth-header__app-name">
-          <img
-            className="auth-header__logo"
-            src="assets/echo_combined_logo_STACKED_fullcolor.svg"
-            alt={t("Title")}
-          />
+          <img className="auth-header__logo" src={logo} alt={t("Title")} />
         </h1>
         <p className="auth-header__greeting">{t("SignIn.Greeting")}</p>
       </header>
