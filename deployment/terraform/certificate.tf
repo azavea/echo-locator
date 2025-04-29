2f872cb8469809133bfa6a48299741cfdd408bf4
Aaron Su <xunzesu@outlook.com>
2025-04-29 16:52:57 -0400

    fixup! update again

--- deployment/terraform/certificate.tf
+++ deployment/terraform/certificate.tf
@@ -1,12 +1,9 @@
 resource "aws_acm_certificate" "cert" {
-  domain_name               = var.r53_public_hosted_zone
-  validation_method         = "DNS"
+  domain_name       = var.r53_public_hosted_zone
+  validation_method = "DNS"
+  key_algorithm     = "RSA_2048"
 
   lifecycle {
     create_before_destroy = true
-  }
-
-  tags = {
-    Renewed = formatdate("YYYYMMDDhhmmss", timestamp()) # Add a timestamp to force a change
   }
 }
