12f7298750ca2971fe08916eacd8214eba03d8a2
Aaron Su <xunzesu@outlook.com>
2025-04-29 15:50:11 -0400

    Terraform: Add cert validation and more configs on cert

--- deployment/terraform/certificate.tf
+++ deployment/terraform/certificate.tf
@@ -1,9 +1,36 @@
 resource "aws_acm_certificate" "cert" {
-  domain_name               = var.r53_public_hosted_zone
-  subject_alternative_names = ["*.${var.r53_public_hosted_zone}"]
-  validation_method         = "DNS"
+  provider          = aws.us-east-1
+  domain_name       = var.r53_public_hosted_zone
+  validation_method = "DNS"
+  statuses          = ["ISSUED"]
+  types             = ["AMAZON_ISSUED"]
+  key_types         = ["RSA_2048"]
+  most_recent       = true
 
   lifecycle {
     create_before_destroy = true
   }
+}
+
+resource "aws_route53_record" "validation_app" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.external.zone_id
+}
+
+resource "aws_acm_certificate_validation" "app" {
+  provider                = aws.us-east-1
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.validation_app : record.fqdn]
 }
