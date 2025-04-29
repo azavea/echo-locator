8e2581ef3311c3c0a4a2f0dc539ef46a678931c9
Aaron Su <xunzesu@outlook.com>
2025-04-29 16:27:54 -0400

    fixup! fix

--- deployment/terraform/certificate.tf
+++ deployment/terraform/certificate.tf
@@ -1,36 +1,13 @@
 resource "aws_acm_certificate" "cert" {
-  provider          = aws.us-east-1
-  domain_name       = var.r53_public_hosted_zone
-  validation_method = "DNS"
-  statuses          = ["ISSUED"]
-  types             = ["AMAZON_ISSUED"]
-  key_types         = ["RSA_2048"]
-  most_recent       = true
+  domain_name               = var.r53_public_hosted_zone
+  subject_alternative_names = ["*.${var.r53_public_hosted_zone}"]
+  validation_method         = "DNS"
 
   lifecycle {
     create_before_destroy = true
   }
-}
 
-resource "aws_route53_record" "validation_app" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
+  tags = {
+    Renewed = timestamp() # Add a timestamp to force a change
   }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.external.zone_id
-}
-
-resource "aws_acm_certificate_validation" "app" {
-  provider                = aws.us-east-1
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.validation_app : record.fqdn]
 }
