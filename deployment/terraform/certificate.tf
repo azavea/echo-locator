e44458d258a0aca41afaddd71b69e89ff964da0c
Aaron Su <xunzesu@outlook.com>
2025-04-29 16:38:14 -0400

    Update certificate

--- deployment/terraform/certificate.tf
+++ deployment/terraform/certificate.tf
@@ -1,6 +1,5 @@
 resource "aws_acm_certificate" "cert" {
   domain_name               = var.r53_public_hosted_zone
-  subject_alternative_names = ["*.${var.r53_public_hosted_zone}"]
   validation_method         = "DNS"
 
   lifecycle {
