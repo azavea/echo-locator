c24b29548ee4a9af0910c9187105b1563bd2c816
Philipp Schmitt <mail@philippschmitt.com>
2025-06-25 15:10:49 +0200

    Tweak toggle button style

--- src/frontend/src/components/base/ToggleButton/ToggleButton.styles.ts
+++ src/frontend/src/components/base/ToggleButton/ToggleButton.styles.ts
@@ -1,15 +1,15 @@
 import { tv } from "tailwind-variants";
 
 export const toggleGroupStyles = tv({
-    base: "inline-flex items-center justify-center rounded-[var(--spacing-4)] border border-gray-300 bg-gray-100",
+    base: "inline-flex items-center justify-center rounded-[var(--spacing-4)] border border-gray-300 bg-gray-100 overflow-hidden",
 });
 
 export const toggleButtonStyles = tv({
-    base: "flex items-center justify-center text-md font-semibold transition-colors outline-none cursor-pointer gap-2.5 rounded-[var(--spacing-4)] px-[10px] py-3",
+    base: "flex items-center justify-center text-md outline-0 outline-gray-300 font-semibold transition-colors cursor-pointer gap-2.5 rounded-[var(--spacing-4)] px-[10px] py-3 focus-visible:ring-2 ring-teal-700 focus-visible:ring-2 ring-inset ring-offset-2 focus-visible:ring-offset-2 focus-visible:ring-offset-white",
     variants: {
         isSelected: {
-            true: "bg-white text-gray-900 shadow-sm",
-            false: "bg-transparent text-gray-600 hover:text-gray-900 border-none",
+            true: "bg-white text-gray-900 shadow-sm outline-1 rounded-[11px]",
+            false: "bg-transparent text-gray-600 hover:text-gray-900 border-none outline-0",
         },
         size: {
             small: "h-[34px] px-[10px] py-3 text-sm",
