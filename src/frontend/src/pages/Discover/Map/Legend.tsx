76c0238ebb4a80b6ffa5999a30a71ec6c49e8b7f
Philipp Schmitt <mail@philippschmitt.com>
2025-08-20 18:17:47 +0200

    wrap legend in another div to center it on mobile

--- src/frontend/src/pages/Discover/Map/Legend.tsx
+++ src/frontend/src/pages/Discover/Map/Legend.tsx
@@ -5,28 +5,37 @@ interface Props {
 }
 
 const Legend = ({ isMobile = true }: Props) => {
-    const { legendContainer, legendItem, legendColorBox, legendLabel } =
-        mapStyles({ isMobile });
+    const {
+        legendWrapper,
+        legendContainer,
+        legendItem,
+        legendColorBox,
+        legendLabel,
+    } = mapStyles({ isMobile });
 
     return (
-        <div className={legendContainer()}>
-            <div className={legendItem()}>
-                <div
-                    className={`${legendColorBox()} bg-[#BCD168] border-1 border-[#748C27]`}
-                />
-                <span className={legendLabel()}>Top 10</span>
-            </div>
-            <div className={legendItem()}>
-                <div
-                    className={`${legendColorBox()} bg-[#D6DFA6] border-1 border-[#8BA045]`}
-                />
-                <span className={legendLabel()}>Recommended</span>
-            </div>
-            <div className={legendItem()}>
-                <div
-                    className={`${legendColorBox()} bg-[#F7F3F1] border-1 border-[#868584]`}
-                />
-                <span className={legendLabel()}>Too far</span>
+        <div className={legendWrapper()}>
+            <div className={legendContainer()}>
+                <div className={legendItem()}>
+                    <div
+                        className={`${legendColorBox()} bg-[#BCD168] border-1 border-[#748C27]`}
+                    />
+                    <span className={legendLabel()}>Top 10</span>
+                </div>
+                <div className={legendItem()}>
+                    <div
+                        className={`${legendColorBox()} bg-[#D6DFA6] border-1 border-[#8BA045]`}
+                    />
+                    <span className={legendLabel()}>Recommended</span>
+                </div>
+                <div className={legendItem()}>
+                    <div
+                        className={`${legendColorBox()} bg-[#F7F3F1] border-1 border-[#868584]`}
+                    />
+                    <span className={`${legendLabel()} font-normal`}>
+                        Too far
+                    </span>
+                </div>
             </div>
         </div>
     );
