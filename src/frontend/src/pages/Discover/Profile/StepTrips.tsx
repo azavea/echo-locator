27bd38838dbbbf4e35409b1d15d441d298adb321
Rachele Morino <rachelekm@gmail.com>
2025-10-14 10:20:13 -0400

    Style add trip step content

--- src/frontend/src/pages/Discover/Profile/StepTrips.tsx
+++ src/frontend/src/pages/Discover/Profile/StepTrips.tsx
@@ -1,6 +1,10 @@
 import { useTranslation } from "react-i18next";
 
+import PlusIcon from "assets/icons/plus.svg?react";
+import TimesIcon from "assets/icons/times.svg?react";
+import Button from "components/base/Button/Button";
 import WizardStep from "components/Wizard/WizardStep";
+import { useState } from "react";
 import type { Destination } from "src/reducers/userProfile/types";
 import type { BaseProps } from "./types";
 
@@ -11,9 +15,35 @@ const StepTrips = ({
     handleNext,
 }: BaseProps) => {
     const { t } = useTranslation();
+    const [addTripModalOpen, setIsAddTripModalOpen] = useState(false);
+    const TEMP_DEST = [
+        {
+            location: {
+                label: "1234 Address Ave Boston 12345",
+                position: {
+                    lat: 1,
+                    lon: 2,
+                },
+            },
+            primary: true,
+            purpose: "Work",
+        },
+        {
+            location: {
+                label: "1234 Address Ave Boston 12345",
+                position: {
+                    lat: 1,
+                    lon: 2,
+                },
+            },
+            primary: false,
+            purpose: "School",
+        },
+    ];
+    const destinations = TEMP_DEST; // buffer.destinations;
 
     const onRemoveDestination = (destinationIndex: number) => {
-        const updatedDestinations = [...buffer.destinations];
+        const updatedDestinations = [...destinations];
         updatedDestinations.splice(destinationIndex, 1);
         setProfileBuffer(state => ({
             ...state,
@@ -24,7 +54,7 @@ const StepTrips = ({
     const onAddDestination = (destination: Destination) => {
         setProfileBuffer(state => ({
             ...state,
-            destinations: [...buffer.destinations, destination],
+            destinations: [...destinations, destination],
         }));
     };
 
@@ -35,11 +65,43 @@ const StepTrips = ({
             buttonText={t("userTrip.wizard.button.finish")}
             handleBack={handleBack}
             handleNext={handleNext}
-            disableNext={!buffer.destinations.length}
+            disableNext={!destinations.length}
         >
-            <p className=" text-gray-600">
-                Step 4. The "Add trip" logic goes here
-            </p>
+            {destinations.map((destination, index) => (
+                <div
+                    key={index}
+                    className="w-full flex flex-row justify-between items-start rounded-2xl p-4 bg-gray-100"
+                >
+                    <div>
+                        <h2 className="text-xl font-bold text-gray-900">
+                            {destination.purpose}
+                        </h2>
+                        <p className="max-w-40 flex flex-wrap font-normal text-gray-600 text-sm">
+                            {destination.location.label}
+                        </p>
+                    </div>
+                    <Button
+                        variant="outline"
+                        size="small"
+                        leftIcon={
+                            <TimesIcon className="h-5 w-5 font-ligth fill-black" />
+                        }
+                        onPress={() => onRemoveDestination(index)}
+                        className="h-7 w-7 py-2 px-3"
+                    />
+                </div>
+            ))}
+            <Button
+                variant="outline"
+                size="large"
+                leftIcon={
+                    <PlusIcon className="h-[18px] w-[18px] fill-gray-600" />
+                }
+                className="justify-normal text-gray-700 text-lg font-bold"
+                onPress={() => setIsAddTripModalOpen(true)}
+            >
+                {t("userTrip.wizard.stepAddTrip.addATrip")}
+            </Button>
         </WizardStep>
     );
 };
